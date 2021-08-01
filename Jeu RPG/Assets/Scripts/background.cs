88d190f8c2d1b42fceab4c5a421c5603ff8a23e6
Gab629 <gab.gauthier2002@gmail.com>
2021-08-01 15:23:23 -0400

    Parrallax fonctionnel avec répétition
    -Il reste un bug pour la répétition

--- Jeu RPG/Assets/Scripts/background.cs
+++ Jeu RPG/Assets/Scripts/background.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class background : MonoBehaviour
 {
-    private float length, startpos;
+    private float length; 
+    private float startpos;
     public GameObject cam;
     public float parallaxEffect;
 
@@ -18,7 +19,16 @@ public class background : MonoBehaviour
     
     void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if( temp > startpos + length)
+        {
+            startpos += length;
+        }else if(temp < startpos - length)
+        {
+            startpos -= length;
+        }
     }
 }
